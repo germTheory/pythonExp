a3e5fa5f286e3ff65d62d2fca7627167f14438ed
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-11-08 14:36:00 -0800

    normalize variable names. Begin work on normalizing inputs.

--- locations.py
+++ locations.py
@@ -4,46 +4,75 @@ import numpy as np
 from pyspark import SparkContext
 from random import randrange
 import math
-import pylab as pl
+# import scipi.interpolate as sp
+import pylab
 
-ebolaid = 1;
-myid = 2;
+ebolaid = 1
+myid = 2
 population = 10
-generations = 100
+iterations = 100
+threshold = .5
 
 def simulatePositions():
-    p = []
-    # 100 people
-    for i in range(population):
-        lat = randrange(90000)/1000.
-        lng = randrange(180000)/1000.
-        p[len(p):] = [(i,lat,lng,0)]
-    l = []
-    for t in range(generations):
-        for j in range(population):
-            p[j]=(
-                p[j][0],
-                p[j][1] + randrange(1000)/1000.,
-                p[j][2] + randrange(1000)/1000.,
-                t)
-            lat = p[j][1]
-            lng = p[j][2]
-            l[len(l):] = [(p[j][0],lat,lng,t)]
-    return l
+    positions = []
+    # Instantiate 100 people
+    for person in range(population):
+        lat = randrange(90000)/1000000.
+        lng = randrange(180000)/1000000.
+        positions[len(positions):] = [ (person, lat, lng, 0) ]
+    arr = []
+    for time in range(iterations):
+        for userId in range(population):
+            positions[userId]=(
+                positions[userId][0],
+                positions[userId][1] + randrange(1000)/100000.,
+                positions[userId][2] + randrange(1000)/100000.,
+                time)
+            lat = positions[userId][1]
+            lng = positions[userId][2]
+            arr[len(arr):] = [(positions[userId][0], lat, lng, time)]
+    return arr
+
+
+# Function will search an array of times for an approximate match
+# All Times stored in unix time
+# def approxBinarySearch():
+
+
+# Function takes a variety of different timed data and returns data points 
+# userData: a set of position data for a specific user(sorted by time)
+# numOfPositions: the number of iterations we want to compare
+# timePeriod: amount of time to analyze, in seconds
+# def normalize(userData, numOfIterations, timePeriod){
+#     data = []
+
+#     times = []
+#     segment = timePeriod / numOfIterations
+#     current = 0
+#     while (current <= timePeriod):
+#         times[len(times):] = current
+#         current += segment
+        
+# }
+
+userData = normalize(infectedUser, 50, 86400)
 
 sc = SparkContext(appName="PythonLocations")
-lis = sc.parallelize(simulatePositions());
-ebolaguy = lis.filter(lambda tup: tup[0]==ebolaid).collect()
-res = lis.map(lambda p: (p[0],math.sqrt((p[1]-ebolaguy[p[3]][1])**2+(p[2]-ebolaguy[p[3]][2])**2),p[3]) )
-mydata = res.filter(lambda tup: tup[0]==myid)
-x = mydata.map(lambda p: p[2]).collect();
-y = mydata.map(lambda p: p[1]).collect();
-# use pylab to plot x and y
-pl.plot(x, y)
-# show the plot on the screen
-pl.show()
 
-result = mydata.reduce(lambda a,b:(myid,a[1]+b[1]))
-print (result[0],result[1]/len(ebolaguy));
+# create an RDD object to store users. This will be replaced with a SQL query as we develop this app
+users = sc.parallelize(simulatePositions())
+infectedUser = users.filter(lambda tup: tup[0] == ebolaid).collect()
+res = users.map(lambda position: (position[0], math.sqrt((position[1] - infectedUser[position[3]][1])**2 + (position[2] - infectedUser[position[3]][2])**2),position[3]) )
+userData = res.filter(lambda tup: tup[0]==myid)
+x = userData.map(lambda position: position[2]).collect();
+y = userData.map(lambda position: position[1]).collect();
+# use pylab to plot x and y
+pylab.plot(x, y)
+pylab.axhline(threshold, 0, iterations)
+# show the plot on the screen
+pylab.show()
+
+result = userData.reduce(lambda a,b:(myid,a[1]+b[1]))
+print (result[0], result[1] / len(infectedUser));
 print 'Finished'
 
