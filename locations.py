72336a1aab05d3261670349e76bc08dc16b8b927
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-11-11 18:37:50 -0800

    Updates locations to temporarily exclude spark.  Adds geopy for location data.

--- locations.py
+++ locations.py
@@ -1,11 +1,12 @@
 
 import sys
 import numpy as np
-from pyspark import SparkContext
+# from pyspark import SparkContext
 from random import randrange
 import math
 import normalize as norm
 import pylab
+from geopy.distance import vincenty
 
 ebolaid = 1
 myid = 2
@@ -25,6 +26,7 @@ def simulatePositions():
     arr = []
     for time in range(iterations):
         for userId in range(population):
+            idx = 0;
             positions[userId]=(
                 positions[userId][0],
                 positions[userId][1] + randrange(1000)/100000.,
@@ -32,24 +34,28 @@ def simulatePositions():
                 time)
             lat = positions[userId][1]
             lng = positions[userId][2]
-            arr[len(arr):] = [(positions[userId][0], lat, lng, time)]
+            arr[len(arr):] = [(positions[userId][0], lat, lng, time, idx)]
     return arr
 
 
-sc = SparkContext(appName="PythonLocations")
+# sc = SparkContext(appName="PythonLocations")
 
 # create an RDD object to store users. This will be replaced with a SQL query as we develop this service
-users = sc.parallelize(simulatePositions())
-
+# users = sc.parallelize(simulatePositions())
+userData = simulatePositions();
 # capture data points of only the two people we are concerned with
-infectedUser = users.filter( lambda tup: tup[0] == ebolaid ).collect()
-comparedUser = users.filter( lambda tup: tup[0] == myid ).collect()
-# normalize data to have consistent data points to compare
-# userData = norm.normalize(infectedUser, 500, startTime, endTime)
-res = users.map(lambda position: (position[0], math.sqrt((position[1] - infectedUser[position[3]][1])**2 + (position[2] - infectedUser[position[3]][2])**2),position[3]) )
-userData = res.filter( lambda tup: tup[0]==myid )
-x = userData.map( lambda position: position[2] ).collect();
-y = userData.map( lambda position: position[1] ).collect();
+infectedUser = userData.filter( lambda tup: tup[0] == ebolaid );
+comparedUser = userData.filter( lambda tup: tup[0] == myid ).collect();
+
+
+# # normalize data to have consistent data points to compare
+# # userData = norm.normalize(infectedUser, 500, startTime, endTime)
+# res = users.map(lambda position: (position[0], math.sqrt((position[1] - infectedUser[position[3]][1])**2 + (position[2] - infectedUser[position[3]][2])**2),position[3]) )
+# userData = res.filter( lambda tup: tup[0]==myid )
+x = infectedUser.map(lambda position: position[3])
+y = []
+for i in userData:
+	y.append()
 # use pylab to plot x and y
 pylab.plot( x, y )
 pylab.axhline( threshold, 0, iterations )
